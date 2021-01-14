38992432c2dc179149c970090a06760e03922acf
Aneeb Hassan <anbk321@gmail.com>
2021-01-14 19:28:24 +0500

    Added the Event/Artist Profile

---
+++ src/Components/Artist/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class ArtistProfile extends React.Component {
+
+        constructor(props) {
+            super(props)
+        }
+        render() {
+            return ( < > < />)
+            }
+        }
